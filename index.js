5c6da30bad0d9dd54697d0c1b930db58242b9f3c
VISAVADIYA KEYUR <104882712+2104030101120@users.noreply.github.com>
2023-09-03 13:02:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function validate(){
     var username=document.getElementById("user").value;
     var password=document.getElementById("pass").value;
 
-    if( username=="keyur" && password=="keyur"){
+    if( username=="admin" && password=="admin@123"){
         alert("LOGIN SUCCESSFUL");
         window.open("home.html");
         return false;
